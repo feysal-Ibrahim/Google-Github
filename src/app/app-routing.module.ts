4843bf6f79ef064f70dc1e9a0915c01a780a1751
feysal-Ibrahim <addictivefazman@gmail.com>
2018-08-23 12:59:00 +0300

    add bootstrap card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { GitDisplayComponent } from './git-display/git-display.component';
+import { GitSearchComponent } from './git-search/git-search.component';
+import { GitRepoComponent } from './git-repo/git-repo.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
